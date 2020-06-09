4e0088c4399682ed4cdab41692b2c54d4546ce6c
Leo Conforti <leonitous.conforti@gmail.com>
2020-06-08 23:32:36 -0500

    initiated CMake

---
+++ build/CMakeFiles/rifp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rifp.dir/test.cpp.o"
+  "rifp.pdb"
+  "rifp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rifp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
